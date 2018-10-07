e685864e6f5434e9a7eb744ade4faa14e4cec4e4
justinasti <rosasmoe@gmail.com>
2018-10-07 09:32:16 +0800

    Lord help me

--- database/migrations/2018_09_22_063315_create_reservations_table.php
+++ database/migrations/2018_09_22_063315_create_reservations_table.php
@@ -20,23 +20,11 @@ class CreateReservationsTable extends Migration
             $table->dateTime('datetime_start');
             $table->dateTime('datetime_end');
             $table->integer('facility_id')->unsigned();
-            $table->integer('requested_by')->unsigned();
-            $table->integer('endorsed_by')->unsigned();     //teacher or adviser approval
-            $table->integer('noted_by')->unsigned();        //facility manager approval
-            $table->integer('approved_by')->unsigned();     //admin approval
             $table->timestamps();
 
             $table->index('facility_id', 'reservations_facility_id_index');
-            $table->index('requested_by', 'reservations_requested_by_index');
-            $table->index('endorsed_by', 'reservations_endorsed_by_index');
-            $table->index('noted_by', 'reservations_noted_by_index');
-            $table->index('approved_by', 'reservations_approved_by_index');
 
             $table->foreign('facility_id', 'reservations_facility_id_foreign')->references('id')->on('facilities');
-            $table->foreign('requested_by', 'reservations_requested_by_foreign')->references('id')->on('users');
-            $table->foreign('endorsed_by', 'reservations_endorsed_by_foreign')->references('id')->on('users');
-            $table->foreign('noted_by', 'reservations_noted_by_foreign')->references('id')->on('users');
-            $table->foreign('approved_by', 'reservations_approved_by_foreign')->references('id')->on('users');
         });
     }
 
@@ -49,16 +37,8 @@ class CreateReservationsTable extends Migration
     {
         Schema::table('reservations', function (Blueprint $table) {
             $table->dropForeign('reservations_facility_id_foreign');
-            $table->dropForeign('reservations_requested_by_foreign');
-            $table->dropForeign('reservations_endorsed_by_foreign');
-            $table->dropForeign('reservations_noted_by_foreign');
-            $table->dropForeign('reservations_approved_by_foreign');
 
             $table->dropIndex('reservations_facility_id_index');
-            $table->dropIndex('reservations_requested_by_index');
-            $table->dropIndex('reservations_endorsed_by_index');
-            $table->dropIndex('reservations_noted_by_index');
-            $table->dropIndex('reservations_approved_by_index');
         });
         Schema::dropIfExists('reservations');
     }
